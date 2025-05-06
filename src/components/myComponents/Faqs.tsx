e8c737ab36826cab55818bd5fcde7cb2bac6bf67
mahatmaArrayyan <muh.mahatma@gmail.com>
2025-05-06 18:37:33 +0800

    feat faqs details

---
+++ src/components/myComponents/Faqs.tsx
@@ -0,0 +1,44 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Card, CardContent, CardHeader } from "../ui/card";
+
+export function Faqs() {
+    return (
+        <Card className="col-span-12">
+			<CardHeader>
+				<h2 className="text-lg font-semibold">Frequently Asked Questions</h2>
+				<p className="text-sm text-muted-foreground">
+					Here are some common questions and answers about our product. 
+				</p>
+			</CardHeader>
+			<CardContent>
+				<Accordion type="single" collapsible className="w-full">
+					<AccordionItem value="item-1">
+						<AccordionTrigger>Is it accessible?</AccordionTrigger>
+						<AccordionContent>
+							Yes. It adheres to the WAI-ARIA design pattern.
+						</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value="item-2">
+						<AccordionTrigger>Is it styled?</AccordionTrigger>
+						<AccordionContent>
+							Yes. It comes with default styles that matches the other
+							components&apos; aesthetic.
+						</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value="item-3">
+						<AccordionTrigger>Is it animated?</AccordionTrigger>
+						<AccordionContent>
+							Yes. It's animated by default, but you can disable it if
+							you prefer.
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			</CardContent>
+        </Card>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/myComponents/Hero";
 import About from "@/components/myComponents/About";
 import Header from "@/components/myComponents/Header";
 import Works from "@/components/myComponents/Works";
+import { Faqs } from "@/components/myComponents/Faqs";
 
 export default function Home() {
 	
@@ -15,13 +16,16 @@ export default function Home() {
 				<About></About>
 			</Container>
 			<Container>
-				<Header title="Works">
-				</Header>
+				<Header title="Works"></Header>
 				<Works></Works>
 			</Container>
 			<Container>
 				<Header title="Blogs"></Header>
 				<Blog />
+			</Container>
+			<Container>
+				<Header title="FAQ's"></Header>
+				<Faqs />
 			</Container>
 		</>
 	);
