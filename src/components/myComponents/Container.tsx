b3b0dcf46a81ef9cfc0b145df6f8112f6d72437c
mahatmaArrayyan <muh.mahatma@gmail.com>
2025-05-20 09:33:42 +0800

    feat: fix container

--- src/components/myComponents/Container.tsx
+++ src/components/myComponents/Container.tsx
@@ -3,7 +3,7 @@ export default function Container({...props}: React.ComponentProps<"div">) {
     return (
         <div
             className="w-9/12 mx-auto grid grid-cols-12 gap-4 mt-20"
-            {...props}
+            {...props} 
         />
     );
 }
